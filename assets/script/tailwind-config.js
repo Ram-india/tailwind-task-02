c11d597a8de0e7ef69937112f425662c03133305
ram <bvn.ramkumar@gmail.com>
2024-11-25 16:51:26 +0530

    initial commit

---
+++ assets/script/tailwind-config.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+    theme: {
+        theme: {
+            fontWeight: {
+              thin: '100',
+              hairline: '100',
+              extralight: '200',
+              light: '300',
+              normal: '400',
+              medium: '500',
+              semibold: '600',
+              bold: '700',
+              extrabold: '800',
+              'extra-bold': '800',
+              black: '900',
+            }
+        },
+      plugins: [require("@tailwindcss/forms")],
+      extend: {
+        colors: {
+          clifford: "#da373d",
+        },
+      },
+    },
+  };
